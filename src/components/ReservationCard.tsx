a6723d411e6ab4c5d270bf276c935950647f0b61
jchanpark <junpark4936@gmail.com>
2022-10-31 23:19:55 -0400

    remove reservation

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeReservation } from '../features/reservationSlice';
 
 interface ReservationCardTypes {
-  name: string
+  name: string;
+  index: number
 }
 
-export default function ReservationCard({ name }: ReservationCardTypes) {
-  return <div className="reservation-card-container">{name}</div>
+export default function ReservationCard({ name, index }: ReservationCardTypes) {
+
+  const dispatch =  useDispatch();
+
+  return <div onClick={() => {
+    dispatch(removeReservation(index))
+  }} className="reservation-card-container">{name}</div>
 }
